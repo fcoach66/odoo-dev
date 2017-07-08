21247fa4c32d737d9ebc12c80ab4702587449a8c
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-08 20:06:36 +0200

    Mod

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+#
+#
+{"name": "To-Do Application",
+ "description": "Manage your personal To-Do tasks.",
+ "version": "10.0.1.0.0",
+ "depends": ["base"],
+ "author": "fcoach66",
+ "installable": True,
+ "active": False, }
