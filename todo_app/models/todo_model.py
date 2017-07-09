57e72212da287823f10e4dc7b7f4de91d6cbb07b
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-09 10:23:44 +0200

    Mod

--- todo_app/models/todo_model.py
+++ todo_app/models/todo_model.py
@@ -10,12 +10,12 @@ class TodoTask(models.Model):
 
 @api.multi 
 def do_toggle_done(self): 
-	for task in self: 
-		task.is_done = not task.is_done 
-return True
-
+    for task in self: 
+        task.is_done = not task.is_done 
+    return True
+	
 @api.model 
 def do_clear_done(self): 
-	dones = self.search([('is_done', '=', True)]) 
-	dones.write({'active': False}) 
-return True
+    dones = self.search([('is_done', '=', True)]) 
+    dones.write({'active': False}) 
+    return True
