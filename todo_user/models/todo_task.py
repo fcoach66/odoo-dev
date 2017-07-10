021021b6c79e09cb03ad5c3733b643525e4badc5
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-10 19:36:49 +0200

    Mod

--- todo_user/models/todo_task.py
+++ todo_user/models/todo_task.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api 
+
 class TodoTask(models.Model): 
     _inherit = 'todo.task' 
     user_id = fields.Many2one('res.users', 'Responsible') 
     date_deadline = fields.Date('Deadline')
+	
+	@api.multi 
+	def do_clear_done(self): 
+    domain = [('is_done', '=', True), 
+               '|', ('user_id', '=', self.env.uid), 
+                    ('user_id', '=', False)] 
+    dones = self.search(domain) 
+    dones.write({'active': False}) 
+    return True
