c801c0fa9dbd8ee1b5273db68d9667a4dd6468eb
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-11 09:14:41 +0200

    Mod

--- todo_user/models/todo_task.py
+++ todo_user/models/todo_task.py
@@ -3,18 +3,16 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class TodoTask(models.Model): 
-    _inherit = 'todo.task' 
+	_inherit = 'todo.task' 
 	user_id = fields.Many2one('res.users', 'Responsible') 
-    date_deadline = fields.Date('Deadline')
+	date_deadline = fields.Date('Deadline')
 	
 	@api.multi 
 	def do_clear_done(self): 
-    domain = [('is_done', '=', True), 
-               '|', ('user_id', '=', self.env.uid), 
-                    ('user_id', '=', False)] 
-    dones = self.search(domain) 
-    dones.write({'active': False}) 
-    return True
+	domain = [('is_done', '=', True), '|', ('user_id', '=', self.env.uid), ('user_id', '=', False)] 
+	dones = self.search(domain) 
+	dones.write({'active': False}) 
+	return True
 	
 	# ...
 	# class TodoTask(models.Model):
@@ -25,4 +23,4 @@ class TodoTask(models.Model):
 			if task.user_id != self.env.user: 
 				raise ValidationError(
 					'Only the responsible can do this!') 
-		return super(TodoTask, self).do_toggle_done()
+	return super(TodoTask, self).do_toggle_done()
