cacf633baada2bf5eb4544391d2287b9edcc5e04
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-11 09:05:59 +0200

    Mod

--- todo_user/models/todo_task.py
+++ todo_user/models/todo_task.py
@@ -2,10 +2,8 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-
 class TodoTask(models.Model): 
     _inherit = 'todo.task' 
-    name = fields.Char(help="What needs to be done?")
 	user_id = fields.Many2one('res.users', 'Responsible') 
     date_deadline = fields.Date('Deadline')
 	
