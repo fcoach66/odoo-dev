15790abae7db5d4fd0db85fd4e3493ec06f35a91
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-12 22:20:29 +0200

    Mod

--- todo_ui/models/todo_model.py
+++ todo_ui/models/todo_model.py
@@ -2,9 +2,9 @@
 from odoo import models, fields, api
 
 class Tag(models.Model):
-  _name = 'todo.task.tag'
-  _description = 'To-do Tag'
-  name = fields.Char('Name', 40, translate=True)
+    _name = 'todo.task.tag'
+    _description = 'To-do Tag'
+    name = fields.Char('Name', 40, translate=True)
 
 class Stage(models.Model): 
     _name = 'todo.task.stage' 
