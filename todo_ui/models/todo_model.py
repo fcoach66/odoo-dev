57fdb75f9f252c9d1bd87ec5da0cb3170b5cb7d6
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-12 15:15:01 +0200

    Mod

--- todo_ui/models/todo_model.py
+++ todo_ui/models/todo_model.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-    from odoo import models, fields, api
+from odoo import models, fields, api
 
-    class Tag(models.Model):
-    _name = 'todo.task.tag'
-    _description = 'To-do Tag'
-    name = fields.Char('Name', 40, translate=True)
+class Tag(models.Model):
+_name = 'todo.task.tag'
+_description = 'To-do Tag'
+name = fields.Char('Name', 40, translate=True)
 
-    class Stage(models.Model):
-    _name = 'todo.task.stage'
-    _description = 'To-do Stage'
-    _order = 'sequence,name'
-    name = fields.Char('Name', 40, translate=True)
-    sequence = fields.Integer('Sequence')
+class Stage(models.Model):
+_name = 'todo.task.stage'
+_description = 'To-do Stage'
+_order = 'sequence,name'
+name = fields.Char('Name', 40, translate=True)
+sequence = fields.Integer('Sequence')
 
