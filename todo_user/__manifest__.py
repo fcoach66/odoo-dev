a9fdc80360692070690360929f572a8b2008ee08
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-12 08:20:47 +0200

    Mod

--- todo_user/__manifest__.py
+++ todo_user/__manifest__.py
@@ -5,7 +5,7 @@
 {'name': 'Multiuser To-Do',
  'description': 'Extend the To-Do app to multiuser.',
  "version": "10.0.1.0.0",
- "depends": ['todo_app'],
+ "depends": ['todo_app', 'mail'],
  "author": 'fcoach66',
  "data": ["views/todo_task.xml"],
  "installable": True,
