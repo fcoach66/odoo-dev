d9341eaee587635aadc0691963d2929fb6e960e6
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-12 14:29:31 +0200

    Mod

--- todo_ui/__manifest__.py
+++ todo_ui/__manifest__.py
@@ -6,7 +6,7 @@
  'name': 'User interface improvements to the To-Do app',
  'description': 'User friendly features.',
  'author': 'fcoach66',
- 'depends': ['todo_user'] 
- "installable": True,
- "active": False
+ 'depends': ['todo_user'], 
+ 'installable': True,
+ 'active': False
 }
