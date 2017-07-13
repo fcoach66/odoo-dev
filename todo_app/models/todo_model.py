502983bbb7d89259afd1aa4e8ead9565cc0bc2a7
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-13 07:58:37 +0200

    Mod

--- todo_app/models/todo_model.py
+++ todo_app/models/todo_model.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-class TodoTask(models.Model): 
-    _name = 'todo.task' 
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
     _description = 'To-do Task'
-    name = fields.Char('Description', required=True) 
-    is_done = fields.Boolean('Done?') 
+    name = fields.Char('Description', required=True)
+    is_done = fields.Boolean('Done?')
     active = fields.Boolean('Active?', default=True)
-    
-    @api.multi 
+
+    @api.multi
     def do_toggle_done(self):
         for task in self:
             task.is_done = not task.is_done
         return True
-	
-    @api.model 
+
+    @api.model
     def do_clear_done(self):
         dones = self.search([('is_done', '=', True)])
         dones.write({'active': False})
